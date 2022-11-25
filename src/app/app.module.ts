6b3f1a42433ff45ea34d4804975b9a5c35c64b42
itsmeabhishekn <abhishekn4454@gmail.com>
2022-11-25 15:38:11 +0530

    added view post

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewPostComponent } from './view-post/view-post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewPostComponent
   ],
   imports: [
     BrowserModule,
